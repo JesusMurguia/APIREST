4bbe36fdb9db12919ce3440116d5cf7f199ff3cc
Jesus Murguia <jesusmurguiafac@gmail.com>
2021-03-17 00:28:43 -0700

    oyea

---
+++ build/generated-sources/ap-source-output/Entity/Alumno_.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-03-17T00:21:31")
+@StaticMetamodel(Alumno.class)
+public class Alumno_ { 
+
+    public static volatile SingularAttribute<Alumno, Integer> idAlumno;
+    public static volatile SingularAttribute<Alumno, Integer> promedio;
+    public static volatile SingularAttribute<Alumno, String> apellido;
+    public static volatile SingularAttribute<Alumno, String> nombre;
+
+}
