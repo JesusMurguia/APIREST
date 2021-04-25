19f0d3e35fc7cc665f160ad4a8b6f3d34a7e8e03
Jesus Murguia <jesusmurguiafac@gmail.com>
2021-04-25 11:39:39 -0700

    nueva entidad

---
+++ build/generated-sources/ap-source-output/Entity/Registrocalificaciones_.java
@@ -0,0 +1,19 @@
+package Entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-25T11:32:51")
+@StaticMetamodel(Registrocalificaciones.class)
+public class Registrocalificaciones_ { 
+
+    public static volatile SingularAttribute<Registrocalificaciones, Integer> idAlumno;
+    public static volatile SingularAttribute<Registrocalificaciones, Float> calificacionParcial;
+    public static volatile SingularAttribute<Registrocalificaciones, String> nombreMateria;
+    public static volatile SingularAttribute<Registrocalificaciones, String> nombreAlumno;
+    public static volatile SingularAttribute<Registrocalificaciones, String> nombreMaestro;
+    public static volatile SingularAttribute<Registrocalificaciones, Integer> id;
+    public static volatile SingularAttribute<Registrocalificaciones, Integer> numParcial;
+
+}
